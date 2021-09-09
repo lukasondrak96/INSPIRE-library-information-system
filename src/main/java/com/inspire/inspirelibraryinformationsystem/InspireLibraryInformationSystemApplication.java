33ebb6588d33da30220920a7de117597c3742ece
ondry96 <ondry.cz@seznam.cz>
2021-09-09 21:58:19 +0200

    Initial commit

---
+++ src/main/java/com/inspire/inspirelibraryinformationsystem/InspireLibraryInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inspire.inspirelibraryinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspireLibraryInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspireLibraryInformationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inspire/inspirelibraryinformationsystem/InspireLibraryInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspire.inspirelibraryinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspireLibraryInformationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
